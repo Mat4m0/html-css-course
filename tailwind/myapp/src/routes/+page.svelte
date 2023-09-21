583dedeeffa0c29d468da154650c193619a25d80
Mat4m0 <matthias.amon@me.com>
2023-09-21 12:30:19 +0200

    init

--- tailwind/myapp/src/routes/+page.svelte
+++
@@ -1,9 +0,0 @@
-<h1 class="text-3xl font-bold underline bg-slate-500">
-  Hello world!
-</h1>
-
-<style lang="postcss">
-  :global(html) {
-    background-color: theme(colors.gray.400);
-  }
-</style>
